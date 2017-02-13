52729cf40b651115f5516fd00bb8726fa07df2fb
Tarik <tarik.abousaddik@gmail.com>
2017-02-13 16:11:02 -0500

    tables have been tested

--- tables.sql
+++ tables.sql
@@ -1,5 +1,21 @@
-CREATE TABLE department(did INT(1) primary key, name VARCHAR(30), num_beds MEDIUMINT); 
-CREATE TABLE employs(eid INT(4) primary key, did);
-CREATE TABLE doctor(eid INT(4) primary key, fees DECIMAL(7,2), specialty VARCHAR(30));
-CREATE TABLE nurse(eid INT(4) primary key, fee DECIMAL(7,2), specialty VARCHAR(30));
-CREATE TABLE patient(med_num INT(4) primary key, first_name VARCHAR(15), last_name VARCHAR(10))
+CREATE TABLE department(
+did INT(1) NOT NULL PRIMARY KEY, 
+dep_name VARCHAR(30), 
+num_beds MEDIUMINT
+); 
+CREATE TABLE employs(
+eid INT(4) NOT NULL PRIMARY KEY, 
+did INT(1) NOT NULL,
+FOREIGN KEY (did) REFERENCES department(did)
+);
+CREATE TABLE doctor(
+eid INT(4) NOT NULL PRIMARY KEY, 
+fees DECIMAL(7,2), 
+specialty VARCHAR(30)
+);
+CREATE TABLE nurse(
+eid INT(4) NOT NULL PRIMARY KEY, 
+fee DECIMAL(7,2), 
+specialty VARCHAR(30)
+);
+-- Missing Patient --
