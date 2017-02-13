563e71d29afe7fa13d4e8219a0c4995578f6c044
Tarik <tarik.abousaddik@gmail.com>
2017-02-13 15:35:59 -0500

    added patient table

--- tables.sql
+++ tables.sql
@@ -3,4 +3,3 @@ CREATE TABLE employs(eid INT(4) primary key, did);
 CREATE TABLE doctor(eid INT(4) primary key, fees DECIMAL(7,2), specialty VARCHAR(30));
 CREATE TABLE nurse(eid INT(4) primary key, fee DECIMAL(7,2), specialty VARCHAR(30));
 CREATE TABLE patient(med_num INT(4) primary key, first_name VARCHAR(15), last_name VARCHAR(10))
-
