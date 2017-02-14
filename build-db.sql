6a40b9800360081b501b44b79000b3f993cbcfeb
Mark Chmilar <markchmilar@gmail.com>
2017-02-14 14:43:55 -0500

    added employee address data

--- build-db.sql
+++ build-db.sql
@@ -179,32 +179,33 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 #########################################################
 ## Insert Employees
 #########################################################
-INSERT INTO employee (eid,first_name,last_name,gender,dob,phone,email,start_date,end_date,job_title) VALUES 
-(100,"Kalia","Drake",0,"2000-07-08","1-110-175-5108","Maecenas@magnaa.net","1999-12-21",null,"doctor"),
-(101,"Tatiana","Sykes",0,"2005-05-06","1-239-473-5797","pede@ipsumcursus.ca","2015-09-27",null,"doctor"),
-(102,"Penelope","Scott",0,"2003-09-16","1-271-490-7841","et@justoProin.net","1997-03-29",null,"doctor"),
-(103,"Kermit","Benson",1,"1965-01-15","1-357-180-4320","arcu@aliquam.org","2004-10-12",null,"doctor"),
-(104,"Levi","Dejesus",1,"1963-11-20","1-292-427-3099","sem.Nulla@orciUt.ca","2000-11-29",null,"doctor"),
-(105,"Hasad","Diaz",1,"1991-02-23","1-288-403-2282","lorem.eget.mollis@egestasFuscealiquet.org","2016-02-26",null,"doctor"),
-(106,"Cora","Pratt",0,"1990-02-15","1-619-264-8381","ac@tincidunt.edu","1999-06-08",null,"doctor"),
-(107,"Evan","Richard",1,"1996-09-12","1-312-227-4544","consectetuer.euismod@Etiamvestibulum.org","2002-12-29",null,"doctor"),
-(108,"Erica","Newman",0,"1984-08-29","1-650-466-4812","placerat.orci@quis.com","2013-07-31",null,"doctor"),
-(109,"Justin","Whatley",0,"1985-07-29","1-650-576-9912","justin@gmail.com","2011-11-29",null,"doctor"),
-(110,"Jaza","Piat",1,"1990-02-23","1-388-567-2282","jaza@gmail.com","2013-05-26","2014-07-07","doctor"),
-(111,"Laura","Leddy",0,"1987-02-15","1-555-264-8381","laura@tincidunt.edu","2009-06-08","2015-03-02","doctor"),
-(112,"Jon","Priar",1,"1992-09-12","1-666-227-4544","jon@Etiamvestibulum.org","2002-12-29","2016-01-01","doctor"),
-(113,"Sophie","Gonzalez",0,"1989-08-29","1-786-166-4812","laura@quis.com","2014-06-30","2015-02-16","doctor"),
-(114,"Tarik","Coolguy",0,"1995-07-29","1-899-576-9912","tarik@gmail.com","2011-11-29","2014-07-08","doctor"),
-(115,"Edan","Weeks",1,"1962-08-05","1-382-124-7929","sollicitudin.adipiscing.ligula@ornarefacilisiseget.edu","2014-02-21",null,"nurse"),
-(116,"Ocean","Barlow",0,"1969-12-27","1-970-423-1665","euismod@doloregestasrhoncus.ca","1998-09-01",null,"nurse"),
-(117,"Raja","Mcdowell",0,"1982-03-13","1-280-666-5439","sit.amet.risus@purusinmolestie.co.uk","1998-09-01",null,"nurse"),
-(118,"Risa","Brady",1,"1960-04-21","1-697-706-1425","eros@amet.co.uk","2002-06-22",null,"nurse"),
-(119,"Celeste","Alvarez",0,"1998-03-10","1-478-266-4522","lacus.Nulla.tincidunt@ametmetusAliquam.co.uk","2012-05-29",null,"nurse"),
-(120,"Aspen","Greer",0,"1962-04-09","1-141-317-6757","elit@velit.ca","2007-02-08","2001-07-18","admin"),
-(121,"Ulric","Richard",1,"1964-08-12","1-445-841-0796","Nullam@adipiscing.net","2009-03-11",null,"admin"),
-(122,"Jocelyn","Nielsen",0,"1988-02-03","1-785-707-6162","dictum.eu@fringillamilacinia.org","1999-06-08",null,"admin"),
-(123,"Mark", "Chmilar",1,"1984-07-14","1-514-812-9449","markchmilar@gmail.com","2010-01-01",null,"admin"),
-(124,"Jackie","Nuc",0,"1984-04-27","1-780-555-4344","jackienuc@gmail.com","2011-01-01",null,"admin");
+
+INSERT INTO employee VALUES
+(100,'Kalia','Drake',0,'2000-07-08','1-110-175-5108','Maecenas@magnaa.net','1999-12-21',NULL,'doctor','100 bishop','Montreal','Canada','H3H1H6'),
+(101,'Tatiana','Sykes',0,'2005-05-06','1-239-473-5797','pede@ipsumcursus.ca','2015-09-27',NULL,'doctor','200 bishop','Montreal','Canada','H2L9D2'),
+(102,'Penelope','Scott',0,'2003-09-16','1-271-490-7841','et@justoProin.net','1997-03-29',NULL,'doctor','300 bishop','Montreal','Canada','H3H1H1'),
+(103,'Kermit','Benson',1,'1965-01-15','1-357-180-4320','arcu@aliquam.org','2004-10-12',NULL,'doctor','400 bishop','Montreal','Canada','H3H1H1'),
+(104,'Levi','Dejesus',1,'1963-11-20','1-292-427-3099','sem.Nulla@orciUt.ca','2000-11-29',NULL,'doctor','111 mckay','Montreal','Canada','H3H1H7'),
+(105,'Hasad','Diaz',1,'1991-02-23','1-288-403-2282','lorem.eget.mollis@egestasFuscealiquet.org','2016-02-26',NULL,'doctor','222 mckay','Montreal','Canada','H3L9S3'),
+(106,'Cora','Pratt',0,'1990-02-15','1-619-264-8381','ac@tincidunt.edu','1999-06-08',NULL,'doctor','333 mckay','Montreal','Canada','H3H1H4'),
+(107,'Evan','Richard',1,'1996-09-12','1-312-227-4544','consectetuer.euismod@Etiamvestibulum.org','2002-12-29',NULL,'doctor','444 mckay','Montreal','Canada','H3H1H9'),
+(108,'Erica','Newman',0,'1984-08-29','1-650-466-4812','placerat.orci@quis.com','2013-07-31',NULL,'doctor','555 mckay','Montreal','Canada','H2K1H1'),
+(109,'Justin','Whatley',0,'1985-07-29','1-650-576-9912','justin@gmail.com','2011-11-29',NULL,'doctor','123 acme','Montreal','Canada','H9H1H1'),
+(110,'Jaza','Piat',1,'1990-02-23','1-388-567-2282','jaza@gmail.com','2013-05-26','2014-07-07','doctor','234 acme','Montreal','Canada','H2S1H1'),
+(111,'Laura','Leddy',0,'1987-02-15','1-555-264-8381','laura@tincidunt.edu','2009-06-08','2015-03-02','doctor','345 acme','Montreal','Canada','H6H1H1'),
+(112,'Jon','Priar',1,'1992-09-12','1-666-227-4544','jon@Etiamvestibulum.org','2002-12-29','2016-01-01','doctor','456 acme','Montreal','Canada','H2H1H1'),
+(113,'Sophie','Gonzalez',0,'1989-08-29','1-786-166-4812','laura@quis.com','2014-06-30','2015-02-16','doctor','567 acme','Montreal','Canada','H5N1H1'),
+(114,'Tarik','Coolguy',0,'1995-07-29','1-899-576-9912','tarik@gmail.com','2011-11-29','2014-07-08','doctor','678 acme','Montreal','Canada','H2J1H1'),
+(115,'Edan','Weeks',1,'1962-08-05','1-382-124-7929','sollicitudin.adipiscing.ligula@ornarefacilisiseget.edu','2014-02-21',NULL,'nurse','111 oiler','Edmonton','Canada','H2Y1H1'),
+(116,'Ocean','Barlow',0,'1969-12-27','1-970-423-1665','euismod@doloregestasrhoncus.ca','1998-09-01',NULL,'nurse','987 queen','Montreal','Canada','H3H1H3'),
+(117,'Raja','Mcdowell',0,'1982-03-13','1-280-666-5439','sit.amet.risus@purusinmolestie.co.uk','1998-09-01',NULL,'nurse','876 queen','Montreal','Canada','H3H1H2'),
+(118,'Risa','Brady',1,'1960-04-21','1-697-706-1425','eros@amet.co.uk','2002-06-22',NULL,'nurse','765 queen','Montreal','Canada','H3H1H8'),
+(119,'Celeste','Alvarez',0,'1998-03-10','1-478-266-4522','lacus.Nulla.tincidunt@ametmetusAliquam.co.uk','2012-05-29',NULL,'nurse','654 queen','Montreal','Canada','H2R1H1'),
+(120,'Aspen','Greer',0,'1962-04-09','1-141-317-6757','elit@velit.ca','2007-02-08','2001-07-18','admin','543 queen','Montreal','Canada','H4H1H1'),
+(121,'Ulric','Richard',1,'1964-08-12','1-445-841-0796','Nullam@adipiscing.net','2009-03-11',NULL,'admin','432 queen','Montreal','Canada','H5H1H1'),
+(122,'Jocelyn','Nielsen',0,'1988-02-03','1-785-707-6162','dictum.eu@fringillamilacinia.org','1999-06-08',NULL,'admin','321 queen','Montreal','Canada','H3H1H5'),
+(123,'Mark','Chmilar',1,'1984-07-14','1-514-812-9449','markchmilar@gmail.com','2010-01-01',NULL,'admin','500 bryant','Montreal','Canada','H7H1H1'),
+(124,'Jackie','Nuc',0,'1984-04-27','1-780-555-4344','jackienuc@gmail.com','2011-01-01',NULL,'admin','600 bryant','Montreal','Canada','H8H1H1');
 
 
 #########################################################
