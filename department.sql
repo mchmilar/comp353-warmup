6865fe0d7b6b9343920ee39495d919f8fdac96a4
Mark Chmilar <markchmilar@gmail.com>
2017-02-13 20:13:08 -0500

    everything consolodated to build-db.sql

--- department.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO department (did,dep_name,num_beds) VALUES (1,"ER",16),(2,"Surgery",81),(3,"Oncology",97),(4,"Burn Unit",76),(5,"Radiology",37);
-
--- nurse.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO nurse (eid, fee, specialty) VALUES
-(115, 99.50, "Triage"),
-(116, 200, "Surgery"),
-(117, 250, "Cancer"),
-(118, 209, "Burns"),
-(119, 175, "X-Rays");
