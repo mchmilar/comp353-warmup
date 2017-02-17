bba1e785dae34aef5dcb8d3f7fd244d484a827b1
Mark Chmilar <mark.chmilar@ericsson.com>
2017-02-17 16:38:03 -0500

    used explicit joins instead of natural joins)

--- marks-wu-queries.sql
+++ marks-wu-queries.sql
@@ -1,48 +1,48 @@
 #1 List the information of all doctors grouped by their departments and specialties.
 SELECT 		*			
-FROM 		departments NATURAL JOIN doctor NATURAL JOIN employee
+FROM 		(department JOIN employee USING(did)) JOIN doctor USING(eid)
 ORDER BY 	dep_name, specialty;
 
 #2 List information about nurses from Montreal who started since Jan 01, 2012.        
 SELECT 	* 
-FROM 	employee NATURAL JOIN nurse 
+FROM 	employee JOIN nurse USING(eid) 
 WHERE 	city='Montreal' AND start_date >= '2012-01-01';
 
 #3 Given a patient’s Medicare ID, list the Medical Report of that patient.
 SELECT 	date_time, first_name, last_name, admin_reason, diagnosis, treatment
-FROM 	visit NATURAL JOIN patient
+FROM 	visit JOIN patient USING(med_num)
 WHERE 	med_num = 1003;
 
 #4 Given a patient’s Medicare ID, determine how much s/he has paid for the visits since the beginning of “this” year.
 SELECT first_name, last_name, total_paid
 FROM patient
-	NATURAL JOIN
+	JOIN
 		(SELECT med_num, sum(fee) as total_paid
-		FROM 		visit NATURAL JOIN doctor
+		FROM 		visit JOIN doctor USING(eid)
 		WHERE 		YEAR(discharged) = YEAR(CURDATE()) 
 		GROUP BY 	med_num
-		) AS visit_fee;
+		) AS visit_fee USING(med_num);
 
 #5 List heart patients admitted/visited at least twice.
 SELECT first_name, last_name, num_visits
 FROM patient
-	NATURAL JOIN (
+	JOIN (
 		SELECT 		med_num, count(diagnosis) as num_visits
         FROM 		visit
         WHERE 		diagnosis='Heart-disease'
         GROUP BY 	med_num
         HAVING 		num_visits >= 2
-        ) AS visit_count;
+        ) AS visit_count USING(med_num);
 
 #6 List patient’s first name, last name, phone, date admitted, date discharged for all
 #  admitted patients grouped by special disease, e.g., Cancer, Heart disease, HIV, SARS, etc.
 SELECT 		diagnosis, first_name, last_name, phone, date_time, discharged 
-FROM 		visit NATURAL JOIN patient 
+FROM 		visit JOIN patient USING(med_num)
 ORDER BY 	diagnosis;
 
 #6b selecting a specific disease. I wasn't sure which one they want.
 SELECT 	diagnosis, first_name, last_name, phone, date_time, discharged 
-FROM 	visit NATURAL JOIN patient 
+FROM 	visit JOIN patient USING(med_num)
 WHERE 	diagnosis='SARS';
 
 
